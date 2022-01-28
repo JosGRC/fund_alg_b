6950648f68e9763da0ad8bedad5a794a9b2cb80f
JosGRC <2109069@upy.edu.mx>
2022-01-28 08:40:23 -0600

    estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global Variables;
+//Create a variable: dataType nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 2;
+float unDecimal = 3.1416;
+char* name = "DanielGarnica";
+char unCaracter = 'D';
+///////////////////////////////
+
+
+/// La ejecucion del programa///
+int main(){
+    printf("Hello Data B %s\n", name);
+
+    return 0;
+}
+///////////////////////////////
