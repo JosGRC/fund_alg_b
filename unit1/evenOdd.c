08bf0eb305bb76f94f7404a1e54ceaecc7a43ef8
JosGRC <2109069@upy.edu.mx>
2022-02-20 01:45:43 -0600

    odd_even

--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -7,6 +7,7 @@ int main(){
     int numberToTest = 9;
     int operation = numberToTest % 2;
     bool hasModule = ( result > 0 );
+    printf("Introduce a number %s\n", name);
 
     if (hasModule) {
 
--- unit1/odd_even.c
+++ unit1/odd_even.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
 
-using namespace std;
-
-int main(){
-    int numero;
-
-    count<<"Digite un numero: ";
-    cin>>numero;
-
-    if(numero==0){
-        cout<<"El numero es par";
+int main(int argc, char **argv) {
+    const char *const even_or_odd[] = { "even", "odd" };
+    for (int i = 1; i < argc; i ++) {
+        const int n = atoi(argv[i]);
+        printf("%d is %s\n",
+               n,
+               even_or_odd[(int)fmod((unsigned)n, 2.0)]);
     }
-    else if(numero%2==0){
-    }
-    else{
-        cout<<"El numero es impar";
-    }
+    return 0;
 }
