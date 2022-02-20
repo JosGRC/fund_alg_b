b31d0d0b3093e9ef49663c709e4f28978e711c5c
JosGRC <2109069@upy.edu.mx>
2022-02-19 19:49:08 -0600

    Odd or even

---
+++ unit1/odd_even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero;
+
+    count<<"Digite un numero: ";
+    cin>>numero;
+
+    if(numero==0){
+        cout<<"El numero es par";
+    }
+    else if(numero%2==0){
+    }
+    else{
+        cout<<"El numero es impar";
+    }
+}
